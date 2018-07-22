6657a047c38d45c72dda83ebe25311c4e61bb257
ActoMatics <sarit891@gmail.com>
2018-07-22 12:57:02 +0300

    Chat app first init

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path'),
+    publicPath = path.join(__dirname, '../public'),
+    express = require('express');
+
+let app = express();    
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+  });
